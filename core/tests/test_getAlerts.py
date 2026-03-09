df9a944a43821c69471eb41373dae06f6d1a28c6
Andrew Korm <z5419859@ad.unsw.edu.au>
2026-03-09 18:36:06 +1100

    check status return

--- core/tests/test_getAlerts.py
+++ core/tests/test_getAlerts.py
@@ -3,9 +3,13 @@ from rest_framework.test import APITestCase
 from rest_framework import status
 
 
-class GetAlertsTests(APITestCase):
+class TestGetAlerts(APITestCase):
 
     fixtures = ["alerts.json"]
 
     def setUp(self):
         self.url = reverse("get-alerts")
+
+    def test_get_alerts_returns_200(self):
+        response = self.client.get(self.url)
+        self.assertEqual(response.status_code, status.HTTP_200_OK)
