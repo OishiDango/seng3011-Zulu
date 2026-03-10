330260dc0262087a000f06ff0f3a9a979973f765
Andrew Korm <z5419859@ad.unsw.edu.au>
2026-03-10 22:33:03 +1100

    added path for stats_regions

--- core/urls.py
+++ core/urls.py
@@ -1,8 +1,10 @@
 from django.urls import path
 from . import views
+from .views import stats_regions
 
 urlpatterns = [
     path('hello/', views.hello_world),
     path('search-def/', views.simple_scrapy_test, name='search_def'),
+    path('stats/regions', stats_regions, name="stats-regions")
 ]
 
