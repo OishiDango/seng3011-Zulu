ae4dfb4d4ef8a1762b4aa7fced814c9a67f7300b
Andrew Korm <z5419859@ad.unsw.edu.au>
2026-03-11 18:00:50 +1100

    created stats_diseases endpoint functionality. Added into the url path. Bug where 'to' parameter is null if 'from' is only passed

--- core/urls.py
+++ core/urls.py
@@ -5,6 +5,7 @@ from .views import stats_regions
 urlpatterns = [
     path('hello/', views.hello_world),
     path('search-def/', views.simple_scrapy_test, name='search_def'),
-    path('stats/regions', stats_regions, name="stats-regions")
+    path('stats/regions', stats_regions, name="stats-regions"),
+    path('stats/diseases', views.stats_diseases, name="stats-diseases")
 ]
 
