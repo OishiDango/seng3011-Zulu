8a204e6f735f04e994195caf55e5a73bd1f7a0f8
Andrew Korm <z5419859@ad.unsw.edu.au>
2026-03-18 22:57:08 +1100

    fix swagger to use https

--- seng3011/urls.py
+++ seng3011/urls.py
@@ -12,6 +12,7 @@ schema_view = get_schema_view(
     ),
     public=True,
     permission_classes=(permissions.AllowAny,),
+    url="https://mbkgairwrk.ap-southeast-2.awsapprunner.com",
 )
 
 urlpatterns = [
